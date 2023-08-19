7d3975b7f5818fa5540baedc7f16e895023b3c86
zouvier <zoumanacisse692@gmail.com>
2023-08-19 01:13:20 -0700

    minor embed change

--- R/embed.R
+++ R/embed.R
@@ -1,25 +1,26 @@
-
-embed <- function(series,dim,lag = 1){
+embed <- function(series, dim, lag = 1){
   
   series <- as.data.frame(series)
   len <- nrow(series)
-  tmp <- seq(1,len,lag)
-  tmp <- tmp[tmp<(len-dim+2)]
+  tmp <- seq(1, len, lag)
+  tmp <- tmp[tmp < (len - dim + 2)]
+  
+  out <- NULL  # Initialize out
   
   for(i in tmp){
+    mtx <- series[i:(dim + (i - 1)), 1]
     
-    mtx <- series[i:(dim+(i-1)),1]
-    
-    if(!exists("out")){
+    if(is.null(out)){
       out <- mtx
-    }else{
-      out <- rbind(out,mtx)
+    } else if (!exists("out")) {
+       out <- mtx
     }
-    
+     else {
+      out <- rbind(out, mtx)
+    }
   }
   
-  out <- t(out)%*%out
+  out <- t(out) %*% out
   
   return(out)
-  
 }
